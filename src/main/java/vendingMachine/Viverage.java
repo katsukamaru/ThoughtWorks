815835abc90c811d1a80b6de7cee7b0cb25ca38e
shin katsumaru <ms.katsumaru@gmail.com>
2018-04-12 14:48:04 +0900

    delete unused comment

--- src/main/java/vendingMachine/Viverage.java
+++ src/main/java/vendingMachine/Viverage.java
@@ -1,9 +1,9 @@
 package vendingMachine;
 
 public class Viverage {
-    private int cokeNum = 1;        // 1
-    private int dietCokeNum = 2;    // 2
-    private int teaNum = 3;         // 3
+    private int cokeNum = 1;
+    private int dietCokeNum = 2;
+    private int teaNum = 3;
     private int viverageType;
 
     Viverage(int viverageType) {
